c51c2cf5f77a70b61f18b9ae9382651522ef816a
LoekaW <loeka.wyseure@student.howest.be>
2021-11-24 12:33:25 +0100

    did styling on the homepage, added color variables

--- src/view/layout.php
+++ src/view/layout.php
@@ -12,10 +12,11 @@
     <li class="nav__item nav__login <?php if($_GET['page']== 'login') {echo 'selected--login';}?>"><?php if(empty($_SESSION['id'])){echo '<a href="index.php?page=login">Log In</a>';} if(!empty($_SESSION['id'])){?><a href="index.php?page=logout">Log Out</a><?php ;} ?></li>
     <li><a class="overview button" href="index.php?page=overview">Overview</a></li>
  <li> <a class="search button" href="index.php?page=search">Search</a></li>
+ <li> <a href="index.php?page=signup" class="button">Sign Up</a></li>
 
 </header>
   <div class="container">
-      <header><h1 class="pagetitle">Watcho</h1></header>
+      <header><h1 class="pagetitle dropshadow">Watcho</h1></header>
       <?php echo $content;?>
   </div>
   <?php echo $js; ?>
--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,18 +1,18 @@
 <header>
-  <a href="index.php?page=signup" class="button">Sign Up</a>
 
-  <br>
-  <br>
+
+
 </header>
-<section class="content">
-
-
+<section class="content intro">
+<h2 class="subtitle">Watch whenever you can</h2>
+<div class="line dropshadow"></div>
+<div >
+<p class="intro__text">Watcho helps you plan your series and movies so you don’t miss any new episodes!</p>
+<p class="intro__text2">Log in to start planning! Not a member yet? Sign up!</p>
+</div>
+<ul class="button--wrapper">
+  <li><a href="index.php?page=login" class="button">Log in</a></li>
+  <li> <a href="index.php?page=signup" class="button button--signup">Sign Up</a></li>
+</ul>
 </section>
-<div>
-  <?php if(!empty($userLogin)){
-    echo $userLogin;
-  }else{
-    echo 'user';
-  }
-  ?>
-  </div>
+
