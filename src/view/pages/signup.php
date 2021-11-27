51b1e1ac9ba153b7b5f629743ad578455b1a8610
LoekaW <loeka.wyseure@student.howest.be>
2021-11-27 15:24:22 +0100

    styling signup part 1

--- src/view/pages/signup.php
+++ src/view/pages/signup.php
@@ -1,40 +1,40 @@
 <article class="signup">
 
  <h1 class="signup__title subtitle">Sign up</h1>
- <section class="border">
+ <section class="border login__wrap">
 
      <form class="signup__form" method="post" action="index.php?page=signup" enctype="multipart/form-data" <?php if(!empty($part1) && $part1==false) {echo 'style="display: none;"';} ?>>
          <input type="hidden" name="action" value="signup">
 
-         <label class="signup__label c1">
-            <span class="form__text">Firstname:</span>
+         <label class="signup__label ">
+            <span class="form__text">What is your firstname?</span>
             <span class="error"><?php if(!empty($errors['name'])){ echo $errors['name'];} ?></span>
-            <input type="text" name="name" class="signup__input signup--name" size="10">
+            <input type="text" class="size1" name="name" class="signup__input signup--name" size="5">
         </label>
-         <label class="signup__label c2">
-            <span class="form__text">Surname:</span>
-            <span class="error"><?php if(!empty($errors['surname'])){ echo $errors['surname'];} ?></span>
-            <input type="text" name="surname" class="signup__input signup--surname">
-        </label>
-         <label class="signup__label c1">
-            <span class="form__text">Username:</span>
-            <span class="error"><?php if(!empty($errors['username'])){ echo $errors['username'];} ?></span>
-            <input type="text" name="username" class="signup__input signup--name">
-        </label>
-        <label class="signup__label c2">
-            <span class="form__text">Email:</span>
+        <label class="signup__label ">
+            <span class="form__text">What is your email adress?</span>
             <span class="error"><?php if(!empty($errors['email'])){ echo $errors['email'];} ?></span>
             <input type="email" name="email" class="signup__input signup--email">
         </label>
-         <label class="signup__label c1">
-            <span class="form__text">Password:</span>
-            <span class="error"><?php if(!empty($errors['password'])){ echo $errors['password'];} ?></span>
-            <input type="password" name="password" class="signup__input signup--password" >
+         <label class="signup__label ">
+            <span class="form__text">What is your surname?</span>
+            <span class="error"><?php if(!empty($errors['surname'])){ echo $errors['surname'];} ?></span>
+            <input type="text" class="size1" name="surname" class="signup__input signup--surname">
+          </label>
+          <label class="signup__label ">
+             <span class="form__text">What is your password?</span>
+             <span class="error"><?php if(!empty($errors['password'])){ echo $errors['password'];} ?></span>
+             <input type="password" class="size1" name="password" class="signup__input signup--password" >
+         </label>
+         <label class="signup__label ">
+            <span class="form__text">How may we call you?</span>
+            <span class="error"><?php if(!empty($errors['username'])){ echo $errors['username'];} ?></span>
+            <input type="text" class="size1" name="username" class="signup__input signup--name">
         </label>
-         <label class="signup__label c2">
-            <span class="form__text">Confirm password:</span>
+         <label class="signup__label ">
+            <span class="form__text">Your password again? </span>
             <span class="error"></span>
-            <input type="password" name="password2" class="signup__input signup--password" >
+            <input type="password" class="size1" name="password2" class="signup__input signup--password" >
         </label>
         <!-- <label class="signup__label c1">
             <span class="form__text">Upload profile picture:</span>
@@ -42,7 +42,7 @@
             <input type="file" name="picture" accept="image/png, image/jpeg, image/gif" class="signup__input" >
         </label> -->
 
-        <input type="submit" class="signup__button" value="NEXT">
+        <input type="submit" class="signup--button button" value="Choose streaming services">
 
 </form>
 
