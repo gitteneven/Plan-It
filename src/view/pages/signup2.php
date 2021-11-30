c6de8111b546c2fd12d9d1a3dc8e6785cfda8e67
LoekaW <loeka.wyseure@student.howest.be>
2021-11-30 11:58:44 +0100

    did styling on signup part 2

--- src/view/pages/signup2.php
+++ src/view/pages/signup2.php
@@ -1,41 +1,41 @@
 <article class="signup">
 
- <h1 class="signup__title">Sign up</h1>
- <section class="">
+ <h1 class="signup__title subtitle">Sign up</h1>
+ <section class="border login__wrap">
 
 
    <form class="signup__form" method="post" action="index.php?page=signup2" enctype="multipart/form-data">
       <input type="hidden" name="action" value="streaming">
-      <h2 class=subtitle>Choose your streaming services:</h2>
+      <!-- <h2 class=subtitle>Choose your streaming services:</h2> -->
 
-      <label class="country input" for="country">Choose your country:
+      <label class="country input" for="country">What country are you from?
     <select id="country" class="country" name="country[]" size="1">
     <?php foreach($countries as $country): ?>
                     <option value="<?php echo $country; ?>"><?php echo $country; ?></option>
                 <?php endforeach; ?>
       </select> <br>
        <span class="error"><?php if(!empty($errors['text'])){ echo $errors['text'];} ?></span>
+    <p class="form__text">What streaming services do you use?</p>
+    <ul name="strInput" id="strInput" class="strList">
 
-    <ul name="strInput" id="strInput" class="strInput">
-    <p class="form__text">Select your streaming services:</p>
+    <li ><label class="">
+    <input type="checkbox" id="netflix" name="strOption[]" value="netflix" class="option str_netflix" >
+    </label></li>
     <li ><label>
-    <input type="checkbox" id="netflix" name="strOption[]" value="netflix" class="option filter" >
-    Netflix</label></li>
-    <li ><label>
-    <input type="checkbox" id="disney+" name="strOption[]" value="disney" class="option filter" >
-    Disney+</label></li>
+    <input type="checkbox" id="disney+" name="strOption[]" value="disney" class="option str_disney" >
+    </label></li>
     <li>
-    <input type="checkbox" id="amazonPrime" name="strOption[]" value="amazon_prime" class="option filter" >
-    Amazon Prime</label></li>
+    <input type="checkbox" id="amazonPrime" name="strOption[]" value="amazon_prime" class="option str_prime" >
+    </label></li>
     <li>
-    <input type="checkbox" id="hbomax" name="strOption[]" value="hbo_max" class="option filter" >
-    HBO Max</label></li>
+    <input type="checkbox" id="hbomax" name="strOption[]" value="hbo_max" class="option str_hbo" >
+    </label></li>
     <li>
-    <input type="checkbox" id="hulu" name="strOption[]" value="hulu" class="option filter" >
-    Hulu</label></li>
+    <input type="checkbox" id="hulu" name="strOption[]" value="hulu" class="option str_hulu" >
+    </label></li>
 
   </ul>
-      <input type="submit" class="signup__button" value="SIGN UP">
+      <input type="submit" class="signup--button button" value="SIGN UP">
   </form>
 </section>
 
